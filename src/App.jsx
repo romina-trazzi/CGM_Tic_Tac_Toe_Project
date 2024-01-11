5f459f2cb4b4766e8ad3192e80eb16660efb7786
Romina Trazzi <romina.trazzi@posta.it>
2024-01-11 18:01:26 +0100

    Progetto completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,143 @@
 import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
+import Log from './components/Log';
+import GameOver from './components/GameOver';
 
+// Gameboard
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+// Winning combinations
+const WINNING_COMBINATIONS = [
+  [
+    { row: 0, column: 0 },
+    { row: 0, column: 1 },
+    { row: 0, column: 2 },
+  ],
+  [
+    { row: 1, column: 0 },
+    { row: 1, column: 1 },
+    { row: 1, column: 2 },
+  ],
+  [
+    { row: 2, column: 0 },
+    { row: 2, column: 1 },
+    { row: 2, column: 2 },
+  ],
+  [
+    { row: 0, column: 0 },
+    { row: 1, column: 0 },
+    { row: 2, column: 0 },
+  ],
+  [
+    { row: 0, column: 1 },
+    { row: 1, column: 1 },
+    { row: 2, column: 1 },
+  ],
+  [
+    { row: 0, column: 2 },
+    { row: 1, column: 2 },
+    { row: 2, column: 2 },
+  ],
+  [
+    { row: 0, column: 0 },
+    { row: 1, column: 1 },
+    { row: 2, column: 2 },
+  ],
+  [
+    { row: 0, column: 2 },
+    { row: 1, column: 1 },
+    { row: 2, column: 0 },
+  ],
+];
+
+// Funzione di helper
+function deriveActivePlayer(turns) {
+  let currentPlayer = 'X';
+
+  if (turns.length > 0 && turns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
+// Componente App
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
-    setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+  // let currentPlayer = 'X';
+  // if (gameTurns.length > 0 && prevTurns[0].player === 'X') {
+  //   currentPlayer = 'O';
+  // }
 
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    // Simbolo del primo quadrato
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+
+    // Simbolo del secondo quadrato
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+
+    // Simbolo del terzo quadrato
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    console.log(combination[0], combination[1], combination[2]);
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      // let currentPlayer = 'X';
+      // if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+      //   currentPlayer = 'O';
+      // }
+
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        {
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer,
+        },
         ...prevTurns,
       ];
 
       return updatedTurns;
     });
   }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id='game-container'>
@@ -38,9 +153,14 @@ function App() {
             symbol='O'
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+
+        {(winner || hasDraw == true) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      LOG
+      <Log activeTurn={gameTurns} />
     </main>
   );
 }
